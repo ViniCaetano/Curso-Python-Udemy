733e542f39fbee7ed3b79f528392f016dce6b24f
Vinicius Guilherme Caetano <vinicaet97@gmail.com>
2022-10-18 18:20:39 -0300

    IPV4
    "int"  object is not callable

---
+++ IPV4/app.py
@@ -0,0 +1,9 @@
+from Classes.calc_ipv4 import CalcIPv4
+
+calc_ipv4 = CalcIPv4(ip = '192.168.0.1', prefixo = 24)
+print(calc_ipv4.ip)
+print(calc_ipv4.mascara)
+print(calc_ipv4.rede)
+print(calc_ipv4.broadcast)
+print(calc_ipv4.prefixo)
+print(calc_ipv4.numero_ips())
