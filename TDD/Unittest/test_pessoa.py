22d0c30fb8f4cd91b185c9e6319a698ab124bf32
Vinicius Guilherme Caetano <vinicaet97@gmail.com>
2022-12-02 14:13:10 -0300

    TDD

---
+++ TDD/Unittest/test_pessoa.py
@@ -0,0 +1,52 @@
+import unittest
+from pessoa import Pessoa
+from unittest.mock import patch
+
+class TestePessoa(unittest.TestCase):
+    def setUp(self):
+        self.p1 = Pessoa('Vinicius', 'Caetano')
+
+    def test_pessoa_attr_nome_tem_o_valor_correto(self):
+        self.assertEqual(self.p1.nome, 'Vinicius')
+
+    def test_pessoa_attr_nome_e_str(self):
+        self.assertIsInstance(self.p1.nome, str)
+    
+    def test_pessoa_attr_sobrenome_tem_o_valor_correto(self):
+        self.assertEqual(self.p1.sobrenome, 'Caetano')
+
+    def test_pessoa_attr_sobrenome_e_str(self):
+        self.assertIsInstance(self.p1.sobrenome, str)
+
+    def test_pessoa_attr_dados_obtidos_inicia_falso(self):
+        self.assertFalse(self.p1.dados_obtidos)
+
+    def test_obter_todos_os_dados_com_sucesso(self):
+        with patch('requests.get') as Fake_request:
+            Fake_request.return_value.ok = True
+
+            self.assertEqual(self.p1.obter_todos_os_dados(), 'CONECTADO')
+            self.assertTrue(self.p1.dados_obtidos)
+
+    def test_obter_todos_os_dados_sem_sucesso(self):
+        with patch('requests.get') as Fake_request:
+            Fake_request.return_value.ok = False
+
+            self.assertEqual(self.p1.obter_todos_os_dados(), 'ERRO 404')
+            self.assertFalse(self.p1.dados_obtidos)
+    def test_obter_todos_os_dados_sem_sucesso_sequencial(self):
+        with patch('requests.get') as Fake_request:
+            Fake_request.return_value.ok = True
+
+            self.assertEqual(self.p1.obter_todos_os_dados(), 'CONECTADO')
+            self.assertTrue(self.p1.dados_obtidos)
+
+            Fake_request.return_value.ok = False
+
+            self.assertEqual(self.p1.obter_todos_os_dados(), 'ERRO 404')
+            self.assertFalse(self.p1.dados_obtidos)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
